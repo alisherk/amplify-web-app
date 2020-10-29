3a895a1b242a1994aad0b20d02f6ab8c70f47651
Alisher K <alisherkabildjanov@yahoo.com>
2020-10-28 23:50:02 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div>
      <h1> Welcome to Sample App build with Amplify </h1>
+     <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
